02a20a6047467141acc7aa3f12a307b0b58e9e40
CanHoangHa <canhoangha2005@gmail.com>
2025-11-20 08:17:28 +0700

    add oauth2 public endpoint

--- eLibrary_service/src/main/java/com/cnpm/eLibrary_service/configuration/SecurityConfig.java
+++ eLibrary_service/src/main/java/com/cnpm/eLibrary_service/configuration/SecurityConfig.java
@@ -46,7 +46,11 @@ public class SecurityConfig {
             "/v3/api-docs",
             "/v3/api-docs/**",
             "/swagger-ui.html",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
+
+            "/oauth2/**",
+            "/login/oauth2/code/**",
+            "/oauth2/authorization/**"
     };
 
     @Bean
